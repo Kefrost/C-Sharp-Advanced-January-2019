ca85273bacfa393a94c4cdd626302a29db9cfc27
Kefrost <50832663+Kefrost@users.noreply.github.com>
2019-05-25 21:25:47 +0300

    Add files via upload

---
+++ Defining Classes/05_Date Modifier/DateModifier.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateModifier
+{
+    public class DateModifier
+    {
+        private DateTime startDate;
+        private DateTime endDate;
+
+        public DateModifier(DateTime start, DateTime end)
+        {
+            this.StartDate = start;
+            this.EndDate = end;
+        }
+
+        public DateTime StartDate
+        {
+            get { return startDate; }
+            set { startDate = value; }
+        }
+
+
+        public DateTime EndDate
+        {
+            get { return endDate; }
+            set { endDate = value; }
+        }
+
+        public double GetDaysDifference()
+        {
+            return Math.Abs((EndDate - startDate).TotalDays);
+        }
+    }
+}
