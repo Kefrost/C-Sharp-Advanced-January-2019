6063d588993f3622d8682ec67e278265a99ef819
Kefrost <50832663+Kefrost@users.noreply.github.com>
2019-05-25 21:14:58 +0300

    Create 13_TriFunction.cs

---
+++ Functional Programming/13_TriFunction/13_TriFunction.cs
@@ -0,0 +1,15 @@
+namespace _13._TriFunction
+{
+    using System;
+    using System.Linq;
+
+    public class TriFunction
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Console.ReadLine().Split().FirstOrDefault(x => x.ToCharArray().Select(y => (int)y).Sum() >= n));
+        }
+    }
+}
