beb5cbe4a5e47ee7fad56a811dd554892cb416ef
Kefrost <50832663+Kefrost@users.noreply.github.com>
2019-05-25 21:35:23 +0300

    Add files via upload

---
+++ Generics/03_Generic Swap Method String/Box.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Swap
+{
+    public class Box<T>
+    {
+        private List<T> data;
+        private int lenght = 0;
+
+        public int Lenght
+        {
+            get { return lenght; }
+        }
+
+
+        public Box()
+        {
+            this.data = new List<T>();
+
+        }
+        public void Add(T item)
+        {
+            this.data.Add(item);
+            lenght++;
+        }
+        public void Swap(int first, int second)
+        {
+            T temp = this.data[first];
+            this.data[first] = this.data[second];
+            this.data[second] = temp;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in this.data)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
