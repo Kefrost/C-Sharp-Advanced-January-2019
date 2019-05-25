f8f84ee42ee6546ebd216b31ad490dbcba92eb79
Kefrost <50832663+Kefrost@users.noreply.github.com>
2019-05-25 21:13:13 +0300

    Create 10_Predicate_Party.cs

---
+++ Functional Programming/10_Predicate Party!/10_Predicate_Party.cs
@@ -0,0 +1,65 @@
+namespace _10._Predicate_Party_
+{
+    using System;
+    using System.Linq;
+
+    public class PredicateParty
+    {
+        public static void Main()
+        {
+            var listOfGuest = Console.ReadLine().Split().ToList();
+
+            while (true)
+            {
+                var command = Console.ReadLine().Split().ToArray();
+
+                if (command[0] == "Party!")
+                {
+                    break;
+                }
+
+                var condition = command[2];
+                Func<string, bool> filter;
+                switch (command[0])
+                {
+                    case "Remove" when command[1] == "StartsWith":
+                        filter = x => !x.StartsWith(condition);
+                        break;
+                    case "Remove" when command[1] == "EndsWith":
+                        filter = x => !x.EndsWith(condition);
+                        break;
+                    case "Remove":
+                        filter = x => x.Length != int.Parse(condition);
+                        break;
+                    case "Double" when command[1] == "StartsWith":
+                        filter = x => x.StartsWith(condition);
+                        break;
+                    case "Double" when command[1] == "EndsWith":
+                        filter = x => x.EndsWith(condition);
+                        break;
+                    default:
+                        filter = x => x.Length == int.Parse(condition);
+                        break;
+                }
+
+                if (command[0] == "Remove")
+                {
+                    listOfGuest = listOfGuest.Where(filter).ToList();
+                }
+                else
+                {
+                    var tempList = listOfGuest.Where(filter).ToList();
+                    foreach (var name in tempList)
+                    {
+                        var index = listOfGuest.IndexOf(name);
+                        listOfGuest.Insert(index, name);
+                    }
+                }
+            }
+
+            Console.WriteLine(listOfGuest.Any()
+                ? $"{string.Join(", ", listOfGuest)} are going to the party!"
+                : "Nobody is going to the party!");
+        }
+    }
+}
