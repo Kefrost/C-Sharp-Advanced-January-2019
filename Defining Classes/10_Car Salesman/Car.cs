dd76177d6f1f5d4fbc975a5cb2a6a1764ab16bbf
Kefrost <50832663+Kefrost@users.noreply.github.com>
2019-05-25 21:31:40 +0300

    Add files via upload

---
+++ Defining Classes/10_Car Salesman/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarSalesman
+{
+    public class Car
+    {
+        public Car(string model, Engine engine)
+        {
+            this.Model = model;
+            this.Engine = engine;
+        }
+
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public string Weight { get; set; }
+        public string Color { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"{this.Model}:");
+            stringBuilder.AppendLine(Engine.ToString());
+            stringBuilder.AppendLine($"  Weight: {this.Weight}");
+            stringBuilder.Append($"  Color: {this.Color}");
+
+            return stringBuilder.ToString();
+
+        }
+    }
+}
---
+++ Defining Classes/10_Car Salesman/Engine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarSalesman
+{
+    public class Engine
+    {
+        public Engine(string model, int power)
+        {
+            this.EngineModel = model;
+            this.Power = power;
+        }
+
+        public string EngineModel { get; set; }
+        public int Power { get; set; }
+        public string Displacement { get; set; }
+        public string Efficiency { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"  {this.EngineModel}:");
+            stringBuilder.AppendLine($"    Power: {this.Power}");
+            stringBuilder.AppendLine($"    Displacement: {this.Displacement}");
+            stringBuilder.Append($"    Efficiency: {this.Efficiency}");
+
+            return stringBuilder.ToString();
+        }
+    }
+}
