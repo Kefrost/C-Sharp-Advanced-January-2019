623ed602e0174d6b729a32ebfe07a397871b2e3d
Kefrost <50832663+Kefrost@users.noreply.github.com>
2019-05-25 21:29:36 +0300

    Add files via upload

---
+++ Defining Classes/08_Raw Data/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RawData
+{
+    public class Car
+    {
+        public Car(string model, Engine carEngine, Cargo carCargo, Tire[] carTires)
+        {
+            this.Model = model;
+            this.CarEngine = carEngine;
+            this.CarCargo = carCargo;
+            this.CarTires = carTires;
+        }
+
+        public string Model { get; set; }
+        public Engine CarEngine { get; set; }
+        public Cargo CarCargo { get; set; }
+        public Tire[] CarTires { get; set; } = new Tire[4];
+    }
+}
