3cc4e19953742ca8248c4b0baa608bcf179d7cd4
Kefrost <50832663+Kefrost@users.noreply.github.com>
2019-05-25 21:22:26 +0300

    Add files via upload

---
+++ Defining Classes/03_Oldest Family Member/Family.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using DefiningClasses;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> people;
+
+        public List<Person> People
+        {
+            get { return this.people; }
+            set { this.people = value; }
+        }
+
+        public void AddMember(Person member)
+        {
+            this.people.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return this.people.OrderByDescending(x => x.Age).FirstOrDefault();
+        }
+    }
+}
