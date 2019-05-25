ac91351c6395f482ec80aef95b36a8b5afcc27d6
Kefrost <50832663+Kefrost@users.noreply.github.com>
2019-05-25 21:28:19 +0300

    Add files via upload

---
+++ Defining Classes/07_Speed Racing/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Speed
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsumption { get; set; }
+        public double TraveledDistance { get; set; }
+
+        public void CheckDistance(double distance)
+        {
+            double amountOfFuelNeeded = this.FuelConsumption * distance;
+
+            if (this.FuelAmount >= amountOfFuelNeeded)
+            {
+                this.FuelAmount -= amountOfFuelNeeded;
+                this.TraveledDistance += distance;
+            }
+            else
+            {
+                System.Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+    }
+}
