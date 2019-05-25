08b06b05e4e89ab704d43878fc2be8a8658a2047
Kefrost <50832663+Kefrost@users.noreply.github.com>
2019-05-25 21:38:46 +0300

    Add files via upload

---
+++ Generics/07_Tuple/Tuple.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TupleDemo
+{
+    public class Tuple<T,K>
+    {
+        private T item1;
+        private K item2;
+
+        public Tuple(T item1, K item2)
+        {
+            this.item1 = item1;
+            this.item2 = item2;
+        }
+        public override string ToString()
+        {
+            return $"{this.item1} -> {this.item2}";
+        }
+    }
+}
