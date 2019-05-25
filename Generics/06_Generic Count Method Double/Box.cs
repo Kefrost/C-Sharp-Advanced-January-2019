9f5e16d566a2087cf859f4f91e5bad18f4fd5430
Kefrost <50832663+Kefrost@users.noreply.github.com>
2019-05-25 21:37:59 +0300

    Add files via upload

---
+++ Generics/06_Generic Count Method Double/Box.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Swap
+{
+    public class Box<T>
+    {
+        private List<T> data;
+
+        public List<T> Data
+        {
+            get { return data; }
+        }
+        
+        public Box()
+        {
+            this.data = new List<T>();
+
+        }
+        public void Add(T item)
+        {
+            this.data.Add(item);
+            
+        }
+        public void Swap(int first, int second)
+        {
+            T temp = this.data[first];
+            this.data[first] = this.data[second];
+            this.data[second] = temp;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in this.data)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
