ecfc8b5f2104bedf2532b73fbd8bbb1636840e06
Kefrost <50832663+Kefrost@users.noreply.github.com>
2019-05-25 21:39:41 +0300

    Add files via upload

---
+++ Generics/08_Threeuple/Threeuple.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TupleDemo
+{
+    public class Threeuple<T,K, V>
+    {
+        private T item1;
+        private K item2;
+        private V item3;
+
+        public Threeuple(T item1, K item2, V item3)
+        {
+            this.item1 = item1;
+            this.item2 = item2;
+            this.item3 = item3;
+        }
+        public override string ToString()
+        {
+            return $"{this.item1} -> {this.item2} -> {this.item3}";
+        }
+    }
+}
