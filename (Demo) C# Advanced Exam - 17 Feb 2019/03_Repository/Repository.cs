93ef104f9d25deac09f4fbe1fb8ad7187197662b
Kefrost <50832663+Kefrost@users.noreply.github.com>
2019-05-25 21:46:48 +0300

    Add files via upload

---
+++ (Demo) C# Advanced Exam - 17 Feb 2019/03_Repository/Repository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Repository
+{
+    public class Repository
+    {
+        private Dictionary<int, Person> data;
+        private int id = 0;
+        public int Count {
+        get
+        { return this.data.Count(); }
+        }
+        public Repository()
+        {
+            this.data = new Dictionary<int, Person>();
+        }
+        public void Add(Person person)
+        {
+            this.data.Add(id, person);
+            id++;
+        }
+        public Person Get(int id)
+        {
+            return data.ElementAt(id).Value;
+        }
+        public bool Update(int id, Person newPerson)    
+        {
+            if (data.ContainsKey(id))
+            {
+                data[id] = newPerson;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Delete(int id)
+        {
+            if (data.ContainsKey(id))
+            {
+                data.Remove(id);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
