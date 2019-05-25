825cffbc643787c00b8f3287f95ca754f8060c9f
Kefrost <50832663+Kefrost@users.noreply.github.com>
2019-05-25 21:34:19 +0300

    Add files via upload

---
+++ Generics/02_Generic Box of Integer/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxofString
+{
+    public class Box<T>
+    {
+        private T value;
+        public T Value
+        {
+            get { return this.value; }
+            set { this.value = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.value.GetType()}: {value}";
+        }
+    }
+}
