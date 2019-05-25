d7a9839774a4217098bab69e4d10e9f90739257a
Kefrost <50832663+Kefrost@users.noreply.github.com>
2019-05-25 21:49:16 +0300

    Add files via upload

---
+++ C# Advanced Exam - 24 February 2019/03_Heroes/Hero.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes
+{
+    public class Hero
+    {
+        public string Name { get; private set; }
+        public int Level { get; private set; }
+        public Item Item { get; private set; }
+
+        public Hero(string name, int level, Item item)
+        {
+            this.Name = name;
+            this.Level = level;
+            this.Item = item;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Hero: {this.Name} – {this.Level}lvl");
+            sb.AppendLine($"Item:");
+            sb.AppendLine($"  * Strength: {this.Item.Strength}");
+            sb.AppendLine($"  * Ability: {this.Item.Ability}");
+            sb.AppendLine($"  * Intelligence: {this.Item.Intelligence}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced Exam - 24 February 2019/03_Heroes/HeroRepository.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Heroes
+{
+    public class HeroRepository
+    {
+        private List<Hero> data;
+
+        public HeroRepository()
+        {
+            data = new List<Hero>();
+        }
+
+        public int Count { get => this.data.Count; }
+
+        public void Add(Hero hero)
+        {
+            data.Add(hero);
+        }
+
+        public void Remove(string name)
+        {
+            var heroToRemove = data.Find(x => x.Name == name);
+            data.Remove(heroToRemove);
+        }
+
+        public Hero GetHeroWithHighestStrength()
+        {
+            int index = 0;
+            int max = 0;
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (data[i].Item.Strength > max)
+                {
+                    max = data[i].Item.Strength;
+                    index = i;
+                }
+            }
+
+            return data[index];
+        }
+
+        public Hero GetHeroWithHighestAbility()
+        {
+            int index = 0;
+            int max = 0;
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (data[i].Item.Ability > max)
+                {
+                    max = data[i].Item.Ability;
+                    index = i;
+                }
+            }
+
+            return data[index];
+        }
+
+        public Hero GetHeroWithHighestIntelligence()
+        {
+            int index = 0;
+            int max = 0;
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (data[i].Item.Intelligence > max)
+                {
+                    max = data[i].Item.Intelligence;
+                    index = i;
+                }
+            }
+
+            return data[index];
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, data);
+        }
+    }
+}
