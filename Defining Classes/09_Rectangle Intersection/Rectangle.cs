ccfbdb22ac6b093eee4112205f2ae23d8cfc0298
Kefrost <50832663+Kefrost@users.noreply.github.com>
2019-05-25 21:30:31 +0300

    Add files via upload

---
+++ Defining Classes/09_Rectangle Intersection/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rectangle
+{
+    public class Rectangle
+    {
+        public Rectangle(string id, double width, double height, double x, double y)
+        {
+            this.Id = id;
+            this.Width = width;
+            this.Height = height;
+            this.TopLeftX = x;
+            this.TopLeftY = y;
+        }
+        public string Id { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double TopLeftX { get; set; }
+        public double TopLeftY { get; set; }
+
+        public bool Intersect(Rectangle secondRectangle)
+        {
+            if (this.TopLeftX + this.Width < secondRectangle.TopLeftX
+                || secondRectangle.TopLeftX + secondRectangle.Width < this.TopLeftX
+                || this.TopLeftY + this.Height < secondRectangle.TopLeftY
+                || secondRectangle.TopLeftY + secondRectangle.Height < this.TopLeftY)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
